2ca89465c5608c9d1837f4066103fe9172d013f5
breadstand <djohnson@breadstand.com>
2021-04-03 13:01:24 -0400

    Playing with Angular. First commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductOfferingsComponent } from './product-offerings/product-offerings.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/product-offerings', pathMatch: 'full'},
+  { path: 'product-offerings', component: ProductOfferingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/product-offerings/product-offerings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-offerings',
+  templateUrl: './product-offerings.component.html',
+  styleUrls: ['./product-offerings.component.css']
+})
+export class ProductOfferingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
